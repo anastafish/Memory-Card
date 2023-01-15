ae98a5433e244a3498c5f9c1d124de1089770b9a
anastafish <anastafish24@gmail.com>
2023-01-15 22:49:18 +0300

    changed the header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import '../styles/header.css'
 export function Header(){
     return(
         <div className="header-container">
-            <h1>Game of Thrones Memory Card</h1>
+            <img src="../../assests/images/pngegg.png" alt="" />
+            <h1>Memory Card</h1>
         </div>
     )
 }
